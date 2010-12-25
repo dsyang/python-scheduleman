fa53d8bb0c9ae478ac6205aa32c14195902ac33a
Dan Yang <dsyang@andrew.cmu.edu>
2010-12-25 00:41:05 -0500

    Revised library to use ics instead of scraping with BeautifulSoup

--- scheduleman/__init__.py
+++ scheduleman/__init__.py
@@ -1,34 +1,10 @@
-#!/usr/bin/env python
-#
-# Copyright 2010, 2011 Sri Raghavan
-#
-# This file is part of python-scheduleman.
-#
-# python-scheduleman is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
 
-# python-scheduleman is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with python-scheduleman.  If not, see <http://www.gnu.org/licenses/>.
-
-"""
-Python client library for The Tartan's ScheduleMan service.
-
-This client library is designed to allow read-only access and parsing of class 
-schedules generated through the ScheduleMan service, in lieu of a true API.
-"""
 import urllib2 as urllib
-from BeautifulSoup import BeautifulSoup
+import string
 import re
 
 """
-ScheduleMan class:
+ScheduleMan ICS class:
 Create an instance of the ScheduleMan class in order to parse a schedule
 from a URL.
 
@@ -44,31 +20,44 @@ class ScheduleMan(object):
     def __init__(self, url=None):
         self.url=url
         self.classes = []
-        self.semester = ""
-        
+        self.semester = None
+        strip_chars = ' \t\n\r\0\x0B\"'
         if self.url is not None:
-            u = urllib.urlopen(url)
-            soup = BeautifulSoup(u)
-            u.close()
+            u = urllib.urlopen(url+ '.ics')
             
-            for cls in soup('li', id=re.compile('^course*')):
-                code = cls.find('h3', 'number').contents[0].strip().replace('-', '')
-                name = cls.find('h4', 'name').contents[0].strip()
-                rec =  cls.find('span', 'selected_section').contents[0].strip()
-                self.classes.append({
-                    'number': code,
-                    'name': name,
-                    'recitation': rec,
-                })
-                
-            semester_text = soup.find('div', 'header').find('h2').contents[0].split()
-            if semester_text[0] == 'Spring':
-                self.semester += 'S'
-            else:
-                self.semester += 'F'
+            #parsing keywords
+            class_code = 'SUMMARY:'
+            class_name = 'DESCRIPTION:'
+            class_endclass = 'END:VEVENT'
+            sem_check = 'RRULE:FREQ=WEEKLY'
+            bad_recs = ('Lec')
             
-            self.semester += semester_text[1]
-            
+            for ln in u.readlines():
+                if(ln.startswith(class_code)):
+                    summary = ln[len(class_code):].strip(strip_chars).replace('-','') #gives XXXXX Lec 3, XXXXX A/B/C/D etc..
+                    rec = summary[6:] #the Lec 3, A/B/C/D part
+                    code = summary[:5] # the XXXXX part
+                    
+                elif(ln.startswith(class_name)):
+                    name = ln[len(class_name):].strip(strip_chars) #gives Introduction to Mathematical Software
+                    
+                elif(ln.startswith(sem_check)):
+                    if(self.semester == None):
+                        temp1 = ln[ln.find('UNTIL='):] #gives a time statement like UNTIL=_20110429_T235959;WKST=SU spring would be 201104, fall: 201112
+                        if(temp1[10] == '0'): #10 is the point right after 2011
+                            self.semester = 'S'
+                        else:
+                            self.semester = 'F'
+                        self.semester += temp1[6:10]
+                        
+                elif(ln.startswith(class_endclass)):
+                    if(not re.match('Lec', rec)): #not a lecture time...
+                        self.classes.append({
+                            'number': code,
+                            'name': name,
+                            'recitation': rec
+                        })
+
         else:
             dom = None
     
@@ -140,11 +129,13 @@ class ScheduleMan(object):
         return '\n'.join([cls.get('number') +' ' + cls.get('recitation') + ' - ' + 
                            cls.get('name') for cls in self.get_schedule()])
     
-def main():
-    sch = ScheduleMan("https://scheduleman.org/schedules/bsYn6rDO1Z")
+def main(url):
+    sch = ScheduleMan(url)
     print sch.get_semester()
     print sch.prettify()
     print "from", sch.get_url()
 
 if __name__ == '__main__':
-    main()
+    url = "https://scheduleman.org/schedules/bsYn6rDO1Z"
+    url = raw_input( "Schedulman URL:")
+    main(url)
